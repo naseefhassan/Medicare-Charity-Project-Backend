be9f1394d5c10721e025de8b8b9d15c3093cb1be
Naseef <naseefhassanm@gmail.com>
2024-03-11 11:34:45 +0530

    forgot password is done, email send through nodemailer

--- Controller/CommonController.js
+++ Controller/CommonController.js
@@ -6,13 +6,14 @@ const twilio = require('../Utility/Twilio');
 const twilioCheck = require('../Utility/TwiloCheck');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 
 
 const object = {
   signupPost: async (req, res) => {
     try {
       const {username, email, password} = req.body;
-      const payload=req.body;
+      const payload = req.body;
 
       const existingUser = await userDetails.findOne({email: email});
 
@@ -27,8 +28,8 @@ const object = {
         }).save();
 
         // gernerating token
-        const expires = 3*24*60*60;
-        const token =jwt.sign({payload},
+        const expires = 3 * 24 * 60 * 60;
+        const token = jwt.sign({payload},
             process.env.SECRET_KEY, {expiresIn: expires},
         );
         res.status(201).json({message: 'User created successfully.', token});
@@ -60,8 +61,8 @@ const object = {
       const check = await userDetails.findOne({email});
       const payload = req.body;
 
-      const expires = 3*24*60*60;
-      const token =jwt.sign({payload},
+      const expires = 3 * 24 * 60 * 60;
+      const token = jwt.sign({payload},
           process.env.SECRET_KEY, {expiresIn: expires},
       );
 
@@ -79,5 +80,62 @@ const object = {
       res.status(500).json({message: 'login error'});
     }
   },
+  forgotPassword: async (req, res) => {
+    try {
+      const {email} = req.body;
+      const user = await userDetails.findOne({email});
+      if (!user) {
+        return res.status(400).json({message: 'User does not exist'});
+      }
+      const expires = 3 * 24 * 60 * 60;
+      const token = jwt.sign({id: user._id}, process.env.SECRET_KEY, {expiresIn: expires});
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.USER_EMAIL,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+
+      const mailOptions = {
+        from: process.env.USER_EMAIL,
+        to: process.env.TO_EMAIL,
+        subject: 'Reset Your password',
+        text: `http://localhost:5173/resetpassword/${user._id}/${token}`,
+      };
+
+      transporter.sendMail(mailOptions, function(error) {
+        console.log('here');
+        if (error) {
+          console.log(error);
+          return res.status(500).json({message: 'Failed to send email'});
+        } else {
+          res.status(200).json({message: 'Success'});
+        }
+      });
+    } catch (error) {
+      console.error('Forgot password error', error);
+      res.status(500).json({message: 'Forgot password error'});
+    }
+  },
+  resetPassword: async (req, res)=>{
+    const {id, token} = req.params;
+    const {password} = req.body;
+
+    jwt.verify(token, process.env.SECRET_KEY, (error, decode)=>{
+      if (error) {
+        return res.json({message: 'invalid token'});
+      } else {
+        bcrypt.hash(password, 10)
+            .then((hash) =>{
+              userDetails.findByIdAndUpdate({_id: id}, {password: hash})
+                  .then((u)=>res.status(200).json({message: 'Success'}))
+                  .catch((err)=>res.status(400).json({message: err}));
+            })
+            .catch((err)=>res.status(500).json({message: err}));
+      }
+    });
+  },
+
 };
 module.exports = object;
--- Router/CommonRouter.js
+++ Router/CommonRouter.js
@@ -3,11 +3,13 @@
 const express=require('express');
 const Router = express.Router();
 const verifyToken=require('../MiddleWare/JWT_Token');
-const {signupPost, verifyOtp, LoginPost} = require('../Controller/CommonController');
+const {signupPost, verifyOtp, LoginPost, forgotPassword, resetPassword} = require('../Controller/CommonController');
 
 
 Router.post('/signupPost', signupPost);
 Router.post('/verifyOtp', verifyOtp);
 Router.post('/LoginPost', LoginPost);
+Router.post('/forgotPassword', forgotPassword);
+Router.post('/resetPassword/:id/:token', resetPassword);
 
 module.exports=Router;
